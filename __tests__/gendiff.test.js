d1b92a53765f93c6ce70b0666e7f1fd8d2dad940
warckut <warckut@gmail.com>
2023-03-08 15:16:00 +0500

    added lint-script

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,26 +1,28 @@
-const { gendiff } = require("../lib/gendiff");
+const { gendiff } = require('../lib/gendiff.js');
 
-let dF1, dF2;
+let dF1;
+let dF2;
+
 beforeEach(() => {
   dF1 = {
-    "host": "hexlet.io",
-    "timeout": 50,
-    "proxy": "123.234.53.22",
-    "follow": false
+    host: 'hexlet.io',
+    timeout: 50,
+    proxy: '123.234.53.22',
+    follow: false,
   };
 
   dF2 = {
-    "timeout": 20,
-    "verbose": true,
-    "host": "hexlet.io"
+    timeout: 20,
+    verbose: true,
+    host: 'hexlet.io',
   };
 });
 
 test('test', () => {
   expect(gendiff(dF1, dF2)).toEqual([
     { key: 'follow', typeDiff: '-', value: false },
-    { key: 'host', typeDiff: ' ', value: "hexlet.io" },
-    { key: 'proxy', typeDiff: '-', value: "123.234.53.22" },
+    { key: 'host', typeDiff: ' ', value: 'hexlet.io' },
+    { key: 'proxy', typeDiff: '-', value: '123.234.53.22' },
     { key: 'timeout', typeDiff: '-', value: 50 },
     { key: 'timeout', typeDiff: '+', value: 20 },
     { key: 'verbose', typeDiff: '+', value: true },
