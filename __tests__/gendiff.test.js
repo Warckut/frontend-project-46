b74d1eb409140c38c8039698c1562c44260fa245
warckut <warckut@gmail.com>
2023-03-28 23:10:16 +0500

    added tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,24 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+
 const { genDiff } = require('../lib/genDiff.js');
 const { parseFile } = require('../lib/parsers.js');
 
-let expectedValue;
-beforeEach(() => {
-  expectedValue = '- follow false\n'
-    + '  host hexlet.io\n'
-    + '- proxy 123.234.53.22\n'
-    + '- timeout 50\n'
-    + '+ timeout 20\n'
-    + '+ verbose true\n';
-});
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 test('genDiff (json)', () => {
-  const df1 = parseFile(`${__dirname}/__fixtures__/file1.json`);
-  const df2 = parseFile(`${__dirname}/__fixtures__/file2.json`);
-  expect(genDiff(df1, df2)).toEqual(expectedValue);
+  const df1 = parseFile(getFixturePath('file1.json'));
+  const df2 = parseFile(getFixturePath('file2.json'));
+  const expected = readFile('stylishFormatResult.txt');
+  expect(genDiff(df1, df2)).toEqual(expected);
 });
 
 test('genDiff (yml/yaml)', () => {
-  const df1 = parseFile(`${__dirname}/__fixtures__/file1.yml`);
-  const df2 = parseFile(`${__dirname}/__fixtures__/file2.yml`);
-  expect(genDiff(df1, df2)).toEqual(expectedValue);
+  const df1 = parseFile(getFixturePath('file1.yml'));
+  const df2 = parseFile(getFixturePath('file2.yml'));
+  const expected = readFile('defaultFormatResult.txt');
+  expect(genDiff(df1, df2)).toEqual(expected);
+});
+
+test('genDiff plain format', () => {
+  const df1 = parseFile(getFixturePath('file1.json'));
+  const df2 = parseFile(getFixturePath('file2.json'));
+  const expected = readFile('plainFormatResult.txt');
+  expect(genDiff(df1, df2, 'plain')).toEqual(expected);
+});
+
+test('genDiff json format', () => {
+  const df1 = parseFile(getFixturePath('file1.json'));
+  const df2 = parseFile(getFixturePath('file2.json'));
+  const expected = readFile('jsonFormatResult.json');
+  expect(genDiff(df1, df2, 'json')).toEqual(expected);
 });
